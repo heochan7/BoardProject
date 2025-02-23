ec85af00e977800cbc3d3faa47819a1ddee1042d
Heochan1125 <geehee1125@gmail.com>
2025-02-23 18:54:04 +0900

    repository ineterface 작성
    이걸로 db 접근

---
+++ boardProject/src/main/java/org/example/boardproject/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package org.example.boardproject.repository;
+
+import org.example.boardproject.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
